d5d22f80d4100d552473ced7cd7d3e5e642e74fc
Chambu311 <achambu@gmail.com>
2025-01-14 20:42:22 -0300

    ts build error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,7 @@ const config = {
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-misused-promises" : "off",
     "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-redundant-type-constituents": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
